14df2880bc5c2059f930161393e5bedf2d2a6096
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-03-20 03:23:33 -0600

    second commit

---
+++ holaMundo.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// las constantes se declaran con let
+let greeting: String = "Hello, World"
+
+// las varibles se declaran con la palabra var
+//Tipos de datos
+var age: Int = 25
+age = 33
+
+let pi: Double = 3.1416
+// Los Doubles pueden contener numeros sin punto decimal
+// pero los Int no pueden contener punto decimal
+let otherDouble: Double = 50
+let isTrue: Bool = true
+let name: String = "Juan Perez"
